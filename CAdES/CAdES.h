e94ede389780d358515df50d881ad346ba635f90
David LeBlanc <dleblanc@microsoft.com>
2018-06-27 19:07:27 -0700

    Add most common extensions

--- CAdES/CAdES.h
+++ CAdES/CAdES.h
@@ -686,22 +686,6 @@ private:
     }
 };
 
-/*
-GeneralName ::= CHOICE {
-otherName                       [0]     OtherName,
-rfc822Name                      [1]     IA5String,
-dNSName                         [2]     IA5String,
-x400Address                     [3]     ORAddress,
-directoryName                   [4]     Name,
-ediPartyName                    [5]     EDIPartyName,
-uniformResourceIdentifier       [6]     IA5String,
-iPAddress                       [7]     OCTET STRING,
-registeredID                    [8]     OBJECT IDENTIFIER }
-// Note - definition for ORAddress is in RFC 3280, and is complex.
-// Hopefully, we won't need it.
-
-*/
-
 typedef Attribute OtherName;
 enum class DirectoryStringType
 {
